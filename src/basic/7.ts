bc67d5dc112cfbb2427a698fac9a4cc037f79edc
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-11-01 16:44:23 +0100

    fix HMTS 1

--- src/basic/7.ts
+++ src/basic/7.ts
@@ -12,7 +12,7 @@ type User = {
 
 
 
-const page1 = {
+const page1: User = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -23,7 +23,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: User = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -2,7 +2,8 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const merged = merge({ name: "Alisa" }, { age: 28 });
+
+const merged = merge({ name: 'Alisa' }, { age: 28 });
 
 merged.name;
 
